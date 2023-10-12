4acfdd799a3c1a7735cc16269e89822f7c15f952
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-13 01:39:33 +0600

    add comment to all setps

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+// for load data others port
 const cors = require("cors");
+// to dynamic changes and start
 const app = express();
+// set dynamic port
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -14,23 +17,28 @@ const users = [
   { id: 3, name: "Shanaj", email: "shanaj@gmail.con" },
 ];
 
+// for home route
 app.get("/", (req, res) => {
   res.send("Users Management server is running");
 });
 
+// for users route
 app.get("/users", (req, res) => {
   res.send(users);
 });
 
+// to get data in client in side
 app.post("/users", (req, res) => {
   console.log("post api hitting");
+  // new user data
   console.log(req.body);
   // get new user
   const newUser = req.body;
   // set id
   newUser.id = users.length + 1;
+  // add newUsers in all users
   users.push(newUser);
-
+  // send new users data to client side
   res.send(newUser);
 });
 
